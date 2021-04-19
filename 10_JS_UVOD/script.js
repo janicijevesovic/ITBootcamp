bc80367b9258846f3ced42d94846c7a7128e8af6
janicijevesovic <janicije95@hotmail.com>
2021-04-19 18:29:36 +0200

    10_Javascript_Uvod

---
+++ 10_JS_UVOD/script.js
@@ -0,0 +1,36 @@
+console.log(3);
+console.log(-15.3867);
+
+console.log("Proizvoljan tekst");
+console.log('Jos jedan tekst');
+console.log("Mark's pen");
+console.log('Mark"s pen');
+console.log('Mark\'s Pen');
+console.log("Mark\"s Pen");
+
+console.log("-15.3867");
+
+
+console.log(3 + 5); // + radi kao sabiranje brojeva
+console.log("3" + "5"); // + radi kao konkatenacija stringova
+console.log(3 + "5"); // broj 3 se konvertuje u string "3", pa se vrsi konkatenacija stringova
+
+
+console.log(true); //logicka vrednost true (tacno)
+console.log(false); //logicka vrednost false (netacno)
+
+let x; // Deklarisanje promenljive x
+console.log("tralala");
+x = 3; // x dobija vrednost 3
+
+console.log(x);
+
+x = 5;
+console.log(x);
+
+x = "Pera Zdera";
+
+let y = true;
+y = 0.5;
+
+console.log(x, y);
