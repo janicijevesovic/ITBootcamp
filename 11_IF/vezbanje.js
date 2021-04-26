d0553f5b19d2b64641ae6caacfee5f67d93f4a3d
janicijevesovic <janicije95@hotmail.com>
2021-04-26 13:59:37 +0200

    23.04.2021

--- 11_IF/vezbanje.js
+++ 11_IF/vezbanje.js
@@ -72,6 +72,7 @@ if (r > p) {
 }
 */
 
+
 // Zadatak 8. Preuzeti koji je dan u nedelji sa računara i ispitati da li je to radni dan ili je u pitanju vikend. 
 let datum = new Date();
 let danasnjiDan = datum.getDay();
@@ -135,5 +136,243 @@ else {
 }
 
 // Zadatak 10. Uporediti dva uneta datuma i ispisati koji od njih je raniji.
-let datumPrvi = new Date (2015, 6, 5);
-let datumDrugi = new Date (2016, 7, 8);
+let datumPrvi = "2005-01-01";
+let datumDrugi = "2005-05-01";
+if(datumPrvi < datumDrugi) {
+    console.log(`${datumPrvi} je bio pre ${datumDrugi}`);
+}
+else if (datumDrugi < datumPrvi) {
+    console.log(`${datumDrugi} je bio pre ${datumPrvi}`);
+}
+else {
+    console.log(`${datumPrvi} i ${datumDrugi} su isti.`);
+}
+
+// 2. nacin
+let dan1 = 01;
+let mes1 = 06;
+let god1 = 2021;
+
+let dan2 = 01;
+let mes2 = 06;
+let god2 = 2021;
+
+if (god1 < god2) {
+    console.log("Datum 1 je raniji.")
+}
+else if (god2 < god1) {
+    console.log("Datum 2 je raniji.");
+}
+// Posto su godine iste, proveravamo za mesece
+else if (mes1 < mes2) {
+    console.log("Datum 1 je raniji.");
+}
+else if (mes2 < mes1) {
+    console.log("Datum 2 je raniji.");
+}
+// Posto su i godine i meseci isti, proveravamo za dane
+else if (dan1 < dan2) {
+    console.log("Datum 1 je raniji.");
+}
+else if (dan2 < dan1) {
+    console.log("Datum 2 je raniji");
+}
+// Posto su i godine i meseci i dani isti datumi su jednaki
+else {
+    console.log("Datum 1 i Datum 2 su isti.");
+}
+
+let stepeni = -10;
+if (stepeni < 0) {
+    console.log("Ispod 0 stepeni");
+}
+else if (stepeni < 10) {
+    console.log("Ispod 10 stepeni");
+}
+else if (stepeni < 20) {
+    console.log("Ispod 20 stepeni");
+}
+else {
+    console.log("20 i vise stepeni");
+}
+
+// Ugnjezdavanje
+
+let pol = "m";
+let god = 32;
+
+if (pol == "z") {
+    console.log("Osoba zenskog pola.");
+    if (god >= 18) {
+        console.log("Osoba je punoletna.");
+    }
+    else {
+        console.log("Osoba je maloletna.");
+    }
+}
+else {
+    console.log("Osoba muskog pola.");
+    if (god >= 18) {
+        console.log("Osoba je punoletna.");
+    }
+    else {
+        console.log("Osoba je maloletna.");
+    }
+}
+
+// 12. Zadatak Za unet početak i kraj radnog vremena dva lekara ispisati DA ukoliko se njihove smene preklapaju, u suprotnom ispisati NE.
+// Pocetak i kraj radnog vremena prvog lekara
+let p1 = 14;
+let k1 = 20;
+
+// Pocetak i kraj radnog vremena drugog lekara
+let p2 = 12;
+let k2 = 19;
+
+if (k1 < p2) {
+    console.log("Ne preklepaju se smene.");
+}
+else if (k2 < p1) {
+    console.log("Ne preklepaju se smene.");
+}
+else {
+    console.log("Preklapaju se smene.");
+}
+
+let broj1 = 33;
+let broj2 = 3;
+if (broj1 % broj2 == 0) {
+    console.log(`${broj1} je deljiv brojem ${broj2}.`);
+}
+else {
+    console.log(`${broj1} nije deljiv brojem ${broj2}.`);
+}
+
+// Zadatak 13. Za uneti broj ispitati da li je paran ili nije. 
+let broj = 18;
+if (broj % 2 == 0) {
+    console.log("Broj je paran");
+}
+else {
+    console.log("Broj je neparan");
+}
+
+// Zadatak 14. Za uneti broj ispisati da li je deljiv sa 3 ili ne.
+broj = 14;
+if (broj % 3 == 0) {
+    console.log("Deljiv sa 3");
+}
+else {
+    console.log("Nije deljiv sa 3");
+}
+
+// Zadatak 15. Za dva uneta broja, od većeg učitanog broja oduzeti manji i rezultat ispisati na ekranu.
+broj1 = 18;
+broj2 = 20;
+if (broj1 > broj2) {
+    console.log(broj1 - broj2);
+}
+else {
+    console.log(broj2 - broj1);
+}
+
+/* Zadatak 16. Za uneti broj ispitati da li je on manji od nule, veći od nule ili jednak nuli. 
+Ukoliko je manji ili jednak nuli ispisati njegov prethodnik, a ukoliko je veći od nule ispisati njegov sledbenik. */
+
+broj = 15;
+if (broj <= 0) {
+    console.log(broj - 1);
+}
+else {
+    console.log(broj + 1);
+}
+
+// Zadatak 17 Za tri uneta broja ispisati koji od njih je najveći, koji od njih je srednji, a koji od nih je najmanji. 
+let p = 15;
+let q = 13;
+let r = 21;
+
+let najveci = p;
+if (q > najveci) {
+    najveci = q;
+}
+if (r > najveci) {
+    najveci = r;
+}
+
+let najmanji = p;
+if (q < najmanji) {
+    najmanji = q;
+}
+if (r < najmanji) {
+    najmanji = r;
+}
+
+if (p > q) {
+    if (q > r) {
+        srednji = q;
+    }
+    else if (p > r) {
+        srednji = r;
+    }
+    else {
+        srednji = p;
+    }
+}
+else {
+    if (p > r) {
+        srednji = p;
+    }
+    else if (q > r) {
+        srednji = r;
+    }
+    else {
+        srednji = q;
+    }
+}
+
+console.log(`Najveci broj je ${najveci}`);
+console.log(`Srednji broj je ${srednji}`);
+console.log(`Najmanji broj je ${najmanji}`);
+
+// Zadatak 18. Za učitani broj ispitati da li je ceo.
+broj = -5.2;
+if (broj % 1 == 0) {
+    console.log(`${broj} je ceo.`);
+}
+else {
+    console.log(`${broj} nije ceo.`);
+}
+
+// Zadatak 19.  Isti kao 17.
+
+
+
+// Zadatak 20. Učitati dva cela broja i ispitati da li je veći od njih paran.
+
+broj1 = 9;
+broj2 = 10;
+if (broj1 > broj2) {
+    if (broj1 % 2 == 0) {
+        console.log(`${broj1} je veci i paran je.`);
+    }
+    else {
+        console.log(`${broj1} je veci i neparan je.`);
+    }
+}
+else if (broj2 > broj1) {
+    if (broj2 % 2 == 0) {
+        console.log(`${broj2} je veci i paran je.`);
+    }
+    else {
+        console.log(`${broj1} je veci i neparan je.`);
+    }
+}
+else {
+    if (broj1 % 2 == 0) {
+        console.log(`Brojevi su jednaki i parni su.`);
+    }
+    else {
+        console.log(`Brojevi su jednaki i neparni su.`);
+    }
+}
