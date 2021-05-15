cd0ebd2383ab51295eacbb6f7ba073ea6785014c
janicijevesovic <janicije95@hotmail.com>
2021-05-15 14:38:19 +0200

    15.05.2021.

--- 17_CALLBACK_FOREACH/vezbanjeForEach.js
+++ 17_CALLBACK_FOREACH/vezbanjeForEach.js
@@ -23,6 +23,15 @@ a.forEach (elem => {
 });
 console.log(`Proizvod je ${proizvod}.`);
 
+let proizvodNiza = celobrojniNiz => {
+    let proizvod = 1;
+    celobrojniNiz.forEach(elem => {
+        proizvod *= elem;
+    });
+    return proizvod;
+}
+console.log(proizvodNiza(a));
+
 // Zadatak 4 Odrediti srednju vrednost elemenata celobrojnog niza.
 console.log(`Srednja vrednost niza je ${suma / a.length}.`);
 
@@ -56,7 +65,9 @@ let maxNiz = niz => {
     return max;
 }
 
-let c = [15, 7, 8, 15, 15, 0, 9, 15, 0, 2];
+
+
+let c = [15, 7, 8, 15, 15, 0, 9, 15, 0, 2, 0];
 console.log(maxNiz(c));
 
 /*
@@ -100,8 +111,7 @@ let brojSvojstvoNiz = (niz, svojstvo) => {
     return broj;
 }
 
-console.log("Broj elemenata sa maksimalnom vrednoscu jednak je: " + brojSvojstvoNiz(c, maxNiz));
-// console.log("Broj elemenata sa minimalnom vrednoscu jednak je: " + brojSvojstvoNiz(c, minNiz));
+
 
 // Zadatak 6 Odrediti minimalnu vrednost u celobrojnom nizu
 
@@ -113,6 +123,19 @@ a.forEach(elem => {
 });
 console.log(`Najmanji clan niza je ${minClan}.`);
 
+let minNiz = niz => {
+    let min = niz[0];
+    niz.forEach(elem => {
+        if (elem < min) {
+            min = elem;
+        }
+    });
+    return min;
+}
+
+console.log("Broj elemenata sa maksimalnom vrednoscu jednak je: " + brojSvojstvoNiz(c, maxNiz));
+console.log("Broj elemenata sa minimalnom vrednoscu jednak je: " + brojSvojstvoNiz(c, minNiz));
+
 // Zadatak 7 Odrediti indeks maksimalnog elementa celobrojnog niza
 maksClan = a[0];
 let indeksMaksClan = 0;
@@ -123,6 +146,20 @@ a.forEach((elem, indeks) => {
     }
 });
 console.log(`Indeks maksimalnog elementa niza je ${indeksMaksClan}`);
+
+let indeksMaksClana = celobrojniNiz => {
+    let maksClan = celobrojniNiz[0];
+    let indeksMaksClan = 0;
+    celobrojniNiz.forEach((elem, indeks) => {
+        if (elem > maksClan) {
+            maksClan = elem;
+            indeksMaksClan = indeks;
+        }
+    });
+    return indeksMaksClan;
+}
+
+console.log(indeksMaksClana(a));
 
 // Zadatak 8 Odrediti indeks minimalnog elementa celobrojnog niza
 minClan = a[0];
